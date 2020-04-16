20b0765c949ec46129ca6498aa041531c66d78cd
shaheelkhan <shaheelkhan59@gmail.com>
2020-04-16 22:47:12 +0530

    Added covid_automated python file

---
+++ covid_automated.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 23 19:53:52 2020
+
+@author: Shaheel
+"""
+
+
+import pandas as pd
+from datetime import datetime
+
+
+confirmed_url = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv"
+death_url = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv"
+recovered_url = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv"
+
+confirmed_data = pd.read_csv(confirmed_url)
+death_data = pd.read_csv(death_url)
+recovered_data = pd.read_csv(recovered_url)
+
+def combined_csv(data1,data2,data3):
+        data = [data1,data2,data3]
+        x =[]
+        
+        for item in data:
+                df = pd.melt(item,id_vars=['Province/State','Country/Region','Lat','Long'],var_name='Updated_date',value_name='cases')
+                x.append(df)
+        confirmed_clean = x[0].rename(columns={'cases':'No:Confirmed'})
+        death_clean = x[1].rename(columns={'cases':'No:Death'})
+        recovered_clean = x[2].rename(columns={'cases':'No:Recovered'})
+        
+        combined = pd.concat([confirmed_clean,death_clean['No:Death'],recovered_clean['No:Recovered']],axis=1)
+        
+        return combined
+
+combined_data = combined_csv(confirmed_data,death_data,recovered_data)
+combined_data['Updated_date'] = pd.to_datetime(combined_data['Updated_date'])
+
+
+combined_data.to_csv('combined_data.csv',index=False,encoding='utf-8-sig')
+
+        
+
